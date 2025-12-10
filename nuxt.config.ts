050f5ca516c8d8fe45c9a15da838b4beb87bb5bd
Florian Streise <fmoessle@yahoo.de>
2025-12-10 09:28:04 +0100

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   routeRules: {
-    '/': {
+    '/**': {
       isr: { allowQuery: [] }
-    },
-    '/slug/**': {
-      isr: { allowQuery: [] }
-    },
+    }
   }
 })
