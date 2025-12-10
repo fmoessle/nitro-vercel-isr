382592294c25ced6b52c398526bece367ff1dcad
Florian Streise <fmoessle@yahoo.de>
2025-12-10 09:25:26 +0100

    test

---
+++ app/pages/slug/[slug].vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>[slug] Page</h1>
+    <pre>route name: {{ route.name }}</pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   routeRules: {
-    '/**': {
+    '/': {
       isr: { allowQuery: [] }
-    }
+    },
+    '/slug/**': {
+      isr: { allowQuery: [] }
+    },
   }
 })
